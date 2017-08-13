141f4284295999d4f24344701096e9ba3b8147a3
Chenyan <397604080@qq.com>
2017-08-14 00:19:11 +0800

    edit plist

---
+++ OKCamera/Classes/Home/Controller/CYHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CYHomeViewController.h
+//  OKCamera
+//
+//  Created by Chenyan on 2017/8/13.
+//  Copyright © 2017年 Chenyan. All rights reserved.
+//
+
+#import "CYViewController.h"
+
+@interface CYHomeViewController : CYViewController
+
+@end
---
+++ OKCamera/Classes/Home/Controller/CYHomeViewController.m
@@ -0,0 +1,22 @@
+//
+//  CYHomeViewController.m
+//  OKCamera
+//
+//  Created by Chenyan on 2017/8/13.
+//  Copyright © 2017年 Chenyan. All rights reserved.
+//
+
+#import "CYHomeViewController.h"
+
+@interface CYHomeViewController ()
+
+@end
+
+@implementation CYHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationItem.title = @"主页";
+}
+
+@end
