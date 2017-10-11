957746171298b65a55309604de9e05b5307f32b0
Chenyan <397604080@qq.com>
2017-10-11 23:37:46 +0800

    add editorController

---
+++ OKCamera/Classes/Main/Lib/CYImageEditor/ViewController/CYEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CYEditorViewController.h
+//  OKCamera
+//
+//  Created by Chenyan on 2017/10/11.
+//  Copyright © 2017年 Chenyan. All rights reserved.
+//
+
+#import "CYViewController.h"
+
+@interface CYEditorViewController : CYViewController
+
+@end
